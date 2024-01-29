cd3a433a4deb54ebb7bb4a24f8ddcfca6bb0b00e
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-01-28 17:16:10 -0800

    Tweaks

--- app/actions.js
+++ app/actions.js
@@ -4,13 +4,14 @@ import { XMLParser } from 'fast-xml-parser'
 
 export async function getReviews() {
 	try {
-		const res = await fetch('https://api.shawn.party/api/just-shillin/reviews', { next: { revalidate: 60 * 60 * 4 } })
+		const res = await fetch('https://api.shawn.party/api/just-shillin/reviews', { next: { revalidate: 60 * 60 * 1 } })
 		const data = await res.json()
 		const { rating, ratingsUrl } = data
 
 		return {
 			appleRating: rating,
 			appleRatingUrl: ratingsUrl,
+			reviews,
 		}
 	} catch {
 		return {}
@@ -20,9 +21,10 @@ export async function getReviews() {
 export async function getSpotifyReviews() {
 	try {
 		const res = await fetch(`https://api.shawn.party/api/pod-data/spotify?url=${'https://open.spotify.com/show/0BM9MOB6jdirna5f1vNcMe'}`, {
-			next: { revalidate: 60 * 60 * 4 },
+			next: { revalidate: 60 * 60 * 1 },
 		})
-		return await res.json()
+		const data = await res.json()
+		return data
 	} catch {
 		return {}
 	}
@@ -39,29 +41,14 @@ export async function getEpisodes() {
 			attributeNamePrefix: '@_',
 		})
 		const parsed = parser.parse(xml)
-		let episodes = []
-		if (parsed.rss.channel.item instanceof Array) {
-			episodes = parsed.rss.channel.item.map(ep => ({
-				guid: ep.guid['#text'],
-				title: ep.title,
-				imgSrc: ep['itunes:image']['@_href'],
-				summary: ep['itunes:summary'],
-				link: ep.link,
-				pubDate: ep.pubDate,
-			}))
-		} else {
-			const ep = parsed.rss.channel.item
-			episodes = [
-				{
-					guid: ep.guid['#text'],
-					title: ep.title,
-					imgSrc: ep['itunes:image']['@_href'],
-					summary: ep['itunes:summary'],
-					link: ep.link,
-					pubDate: ep.pubDate,
-				},
-			]
-		}
+		const episodes = parsed.rss.channel.item.map(ep => ({
+			guid: ep.guid['#text'],
+			title: ep.title,
+			imgSrc: ep['itunes:image']['@_href'],
+			summary: ep['itunes:summary'],
+			link: ep.link,
+			pubDate: ep.pubDate,
+		}))
 		return {
 			episodes,
 		}
