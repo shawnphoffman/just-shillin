b4c311e40069f81757e89827251264a9c3eb550f
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-01-30 20:31:53 -0800

    Add search

--- app/(pages)/episodes/page.js
+++ app/(pages)/episodes/page.js
@@ -1,7 +1,8 @@
 import { Suspense } from 'react'
 
 import { getEpisodes } from 'app/actions'
-import Episode from 'components/Episode'
+// import Episode from 'components/Episode'
+import Episodes from 'components/Episodes'
 import Loading from 'components/Loading'
 
 export const revalidate = 60 * 60 // 1 hour
@@ -13,7 +14,9 @@ const EpisodesClient = async () => {
 		//
 		// new Promise(resolve => setTimeout(resolve, 5000)),
 	])
-	return data.episodes.map(ep => <Episode episode={ep} key={ep.guid} />)
+	// return data.episodes.map(ep => <Episode episode={ep} key={ep.guid} />)
+
+	return <Episodes episodes={data.episodes} />
 }
 
 export default async function EpisodesPage() {
