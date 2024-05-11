1f5b3c20a6c842b5d734bd385bbc5a3f3b96250f
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-11 11:08:36 -0700

    Fixes

--- sanity/schemaTypes/category.ts
+++ sanity/schemaTypes/category.ts
@@ -1,19 +1,34 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from 'sanity'
 
 export default defineType({
-  name: 'category',
-  title: 'Category',
-  type: 'document',
-  fields: [
-    defineField({
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    }),
-    defineField({
-      name: 'description',
-      title: 'Description',
-      type: 'text',
-    }),
-  ],
+	name: 'category',
+	title: 'Category',
+	type: 'document',
+	fields: [
+		defineField({
+			name: 'title',
+			title: 'Title',
+			type: 'string',
+		}),
+		defineField({
+			name: 'description',
+			title: 'Description',
+			type: 'text',
+		}),
+		defineField({
+			name: 'image',
+			title: 'Image',
+			type: 'image',
+			options: {
+				hotspot: true,
+			},
+			fields: [
+				{
+					name: 'alt',
+					type: 'string',
+					title: 'Alternative Text',
+				},
+			],
+		}),
+	],
 })
