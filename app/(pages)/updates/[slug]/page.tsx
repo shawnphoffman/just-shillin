0d5ebb5f446378a28a24db5431318a59558c176e
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-12 12:44:49 -0700

    9

--- app/(pages)/updates/[slug]/page.tsx
+++ app/(pages)/updates/[slug]/page.tsx
@@ -3,8 +3,14 @@ import { notFound } from 'next/navigation'
 import PostBody from '@/components/updates/PostBody'
 import { getAllPostsSlugs, getPostBySlug } from '@/utils/sanity/sanity.client'
 
-export default async function PostPage({ params }: { params: { slug: string } }) {
-	const post = await getPostBySlug(params.slug)
+type PageProps = {
+	params: {
+		slug: string
+	}
+}
+
+export default async function PostPage({ params }: PageProps) {
+	const post = await getPostBySlug(params?.slug || '')
 
 	// console.log(post)
 
@@ -21,10 +27,10 @@ export default async function PostPage({ params }: { params: { slug: string } })
 				<PostBody content={post.body} />
 			</article>
 
-			<hr />
+			{/* <hr />
 			<pre style={{ textAlign: 'left', fontSize: 10 }}>
 				<code>{JSON.stringify(post, null, 2)}</code>
-			</pre>
+			</pre> */}
 		</>
 	)
 }
