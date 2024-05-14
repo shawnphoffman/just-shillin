95704b33884a315e20b5a779a7f60262f6343a8e
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-14 07:18:57 -0700

    Styling

--- components/core/LinkCard.tsx
+++ components/core/LinkCard.tsx
@@ -1,14 +1,14 @@
 import styles from './LinkCard.module.css'
 
 export default function LinkCard({ link, bg, icon, title, color = 'var(--white)' }) {
-	const gradient = `linear-gradient(${bg}, ${bg}), linear-gradient(135deg, white, grey, white) !important`
+	// const gradient = `linear-gradient(${bg}, ${bg}), linear-gradient(135deg, white, grey, white) !important`
 	return (
 		<a
-			className={`${styles.container} wow`}
+			className={`${styles.container} wow border-4 border-gray-300`}
 			href={link}
 			target="_blank"
 			rel="noopener noreferrer"
-			style={{ backgroundImage: gradient, backgroundColor: bg, color: color }}
+			style={{ backgroundColor: bg, color: color }}
 		>
 			<i className={icon} aria-hidden />
 			<h2 className={styles.title}>{title}</h2>
