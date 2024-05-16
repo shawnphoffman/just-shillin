dd4ad76166434462fe4085e11294aa3e79261fbc
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-16 11:38:11 -0700

    not found & loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,10 @@
+import { type IconProp } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function Loading() {
-	return <div className="fa-solid fa-space-station-moon-construction fa-2xl fa-beat-fade" />
+	return (
+		<div className="text-4xl text-white">
+			<FontAwesomeIcon icon={'fa-solid fa-space-station-moon-construction' as IconProp} beatFade />
+		</div>
+	)
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,26 @@
+import { faArrowRight } from '@awesome.me/kit-d7ccc5bb1a/icons/classic/solid'
+import { faAndy2, faShawn2 } from '@awesome.me/kit-d7ccc5bb1a/icons/kit/custom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
-
-// TODO FIX THIS
 
 export default function NotFound() {
 	return (
-		<div>
-			<h2>Not Found</h2>
-			<p>
-				<Link href="/">Go home</Link>
-			</p>
+		<div className="flex flex-col items-center justify-center max-w-2xl gap-8 mx-auto text-white h-dvh w-dvw">
+			<div className="flex flex-col w-full gap-2">
+				<h1 className="text-6xl font-bold text-yellow-400">Uh oh...</h1>
+				<h2 className="text-xl text-white/80">These aren&apos;t the droids you&apos;re looking for...</h2>
+				<Link
+					href="/"
+					className="flex flex-row items-center justify-center gap-2 text-2xl font-bold group hover:text-yellow-400 text-sky-400 w-fit"
+				>
+					<span className="group-hover:bg-squiggle">Return to safety</span>
+					<FontAwesomeIcon icon={faArrowRight} />
+				</Link>
+			</div>
+			<div className="text-red-600 animate-pulse text-[20rem] flex flex-row gap-4 justify-center flex-wrap">
+				<FontAwesomeIcon icon={faAndy2} />
+				<FontAwesomeIcon icon={faShawn2} />
+			</div>
 		</div>
 	)
 }
