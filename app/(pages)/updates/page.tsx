1ef6be37a044a9c727a35547c174462a55f6fbd2
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-26 11:46:16 -0400

    updates to match scruffies

--- app/(pages)/updates/page.tsx
+++ app/(pages)/updates/page.tsx
@@ -1,5 +1,6 @@
 import PostRow from '@/components/updates/PostRow'
 import { getAllPosts } from '@/sanity/sanity.requests'
+import { POST_QUERYResult } from '@/sanity/sanity.types'
 
 export default async function UpdatesPage() {
 	const posts = await getAllPosts()
@@ -8,8 +9,7 @@ export default async function UpdatesPage() {
 		<div className="w-full max-w-3xl mb-8 border rounded-lg border-zinc-900 bg-zinc-950/75">
 			<div className="flex flex-col justify-center w-full p-2 divide-y divide-sky-500">
 				{posts.map(post => {
-					const { author, mainImage, publishedAt, slug, _id, title } = post
-					return <PostRow key={_id} author={author} mainImage={mainImage} publishedAt={publishedAt} slug={slug} title={title} />
+					return <PostRow key={post._id} post={post as NonNullable<POST_QUERYResult>} />
 				})}
 			</div>
 		</div>
