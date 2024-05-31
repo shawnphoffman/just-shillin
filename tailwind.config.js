c15995d0e9137cf6d3cbc1d4799692952b62bed9
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-05-31 12:48:55 -0400

    tailwind pod colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+import tailwindColors from '@shawnphoffman/pod-sites-common/tailwind'
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
@@ -20,31 +22,7 @@ module.exports = {
 			animation: {
 				fadeInUp: '0.5s fadeInUp',
 			},
-			backgroundColor: {
-				applepodcasts: '#872ec4',
-				spotify: '#16883e',
-				twitter: '#0d7ac4',
-				teepublic: '#374ecd',
-				overcast: '#be5a01',
-				youtube: '#ee0000',
-				googlepodcasts: '#206ff2',
-				goodpods: '#fcdb00',
-				amazonmusic: '#0077c1',
-				iheart: '#c6002b',
-				pocketcasts: '#ea150d',
-				castbox: '#db3c0e',
-				zencastr: '#1474e0',
-				rss: '#ce4a0c',
-				facebook: '#0866ff',
-				bluesky: '#0560ff',
-				instagram: '#c13584',
-				etsy: '#f56400',
-				radiopublic: '#ce262f',
-				podbean: '#5f7e1b',
-				googlepodcasts: '#206ff2',
-				email: '#52565e',
-			},
 		},
 	},
-	plugins: [],
+	plugins: [tailwindColors.default],
 }
