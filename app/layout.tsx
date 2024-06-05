d995b77ccd7848cbf547e2ae3b3eb576e697ae50
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-05 15:20:06 -0400

    scroll fix and layout consolidation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import '@/app/global.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import '@/app/(pages)/icons'
 
 import { GeistSans } from 'geist/font/sans'
+import Image from 'next/image'
 
 import { siteDescription, siteTitle, siteUrl } from '@/app/meta'
+import headerImage from '@/app/title.png'
+import ActiveLink from '@/components/core/ActiveLink'
+import StarBackground from '@/components/core/StarBackground'
 
 export const metadata = {
 	title: {
@@ -27,11 +32,30 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
 	return (
-		<html lang="en" className={GeistSans.className}>
+		<html lang="en" className={`${GeistSans.className} bg-black h-full p-0 m-0 overflow-x-hidden  w-dvw`}>
 			<head>
 				<meta name="apple-itunes-app" content="app-id=1726695035" />
 			</head>
-			<body>{children}</body>
+			<body className="px-2 py-0 mx-auto my-0 text-white min-h-dvh w-dvw">
+				<StarBackground />
+				<div className="flex flex-col items-center w-full max-w-screen-xl mx-auto">
+					<div className="flex flex-col w-full max-w-4xl min-h-dvh">
+						<div className="flex flex-col items-center m-4 text-center">
+							<h1 className="sr-only">{siteTitle}</h1>
+							{/* IMAGE */}
+							<Image className="w-52 lg:w-72" alt="" src={headerImage} width={300} height={300} priority />
+							{/* NAV */}
+							<nav className="flex flex-row flex-wrap justify-center gap-4">
+								<ActiveLink href="/" label="Links" />
+								<ActiveLink href="/updates" label="Updates" />
+								<ActiveLink href="/episodes" label="Episodes" />
+								<ActiveLink href="/listen-now" label="Listen Now" />
+							</nav>
+						</div>
+						<main className="flex flex-col items-center flex-1 gap-4 text-center">{children}</main>
+					</div>
+				</div>
+			</body>
 		</html>
 	)
 }
