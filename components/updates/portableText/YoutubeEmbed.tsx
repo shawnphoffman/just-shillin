a6979cfc3729add5944b29e9153a68c191b494b0
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-11 18:01:26 -0400

    yt border

--- components/updates/portableText/YoutubeEmbed.tsx
+++ components/updates/portableText/YoutubeEmbed.tsx
@@ -26,7 +26,7 @@ const LazyYoutube = ({ videoId }) => {
 	}, [])
 
 	return (
-		<div ref={videoRef} className="my-6 mx-4">
+		<div ref={videoRef} className="mx-4 my-6 overflow-hidden border rounded-lg border-zinc-900 bg-zinc-950/75 hover:border-zinc-500">
 			{load ? (
 				<iframe
 					width="100%"
