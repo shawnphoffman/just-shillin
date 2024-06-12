91e5854c1035ca00088f06702ad6831967806e01
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-12 13:56:56 -0400

    post row styling

--- components/updates/PostRow.tsx
+++ components/updates/PostRow.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import { urlForSanityImage } from '@/sanity/sanity.image'
 import { POST_QUERYResult } from '@/sanity/sanity.types'
 
-import PostAuthor from './PostAuthor'
 import PostAuthorAvatar from './PostAuthorAvatar'
 import PostDate from './PostDate'
 
@@ -14,7 +13,7 @@ type Props = {
 export default function PostRow({ post }: Props) {
 	const { author, mainImage, publishedAt, slug, title } = post
 	return (
-		<a href={`/updates/${slug}`} className="w-full mx-auto text-left text-white md:w-full group max-md:max-w-96">
+		<a href={`/updates/${slug}`} className="w-full mx-auto text-left text-white group md:w-full max-md:max-w-96 hover:bg-zinc-900/75">
 			<div className="flex flex-col items-center justify-between gap-2 px-0 py-4 transition-all rounded-lg md:py-2 md:px-4 md:flex-row md:gap-4">
 				{mainImage && (
 					<Image
@@ -31,15 +30,15 @@ export default function PostRow({ post }: Props) {
 						<PostAuthorAvatar name={author?.name} image={author?.image} />
 					</div>
 					<div className="flex flex-col items-start justify-center flex-1 w-full gap-1">
-						<h2 className="text-xl font-bold leading-tight transition-colors group-hover:text-brand2">{title}</h2>
-						{/* <span className="md:hidden"> */}
+						<h2 className="text-xl font-bold leading-tight transition-all group-hover:bg-squiggle group-hover:text-brand-yellow group-hover:text-brand2">
+							{title}
+						</h2>
 						<PostDate dateString={publishedAt} />
-						{/* </span> */}
 						<span className="text-sm leading-tight text-white/90">{post.excerpt}</span>
 					</div>
 				</div>
 				<div className="hidden md:flex">
-					<PostAuthor name={author?.name} image={author?.image} />
+					<PostAuthorAvatar name={author?.name} image={author?.image} />
 				</div>
 			</div>
 		</a>
