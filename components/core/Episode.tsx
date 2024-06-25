8ced0582d70fee496a1dcbce4b4900e8748c7a20
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-06-25 15:43:52 -0700

    Episode links

--- components/core/Episode.tsx
+++ components/core/Episode.tsx
@@ -1,4 +1,7 @@
+import { faLink } from '@awesome.me/kit-d7ccc5bb1a/icons/classic/solid'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Image from 'next/image'
+import slugify from 'slugify'
 
 import EpisodeSummary from './EpisodeSummary'
 
@@ -6,9 +9,15 @@ const options = { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'Ame
 
 export default function Episodes({ episode }) {
 	const pubDate = new Date(episode.pubDate).toLocaleDateString('en-US', options)
+	const slug = slugify(episode.title)
 	return (
 		<article className="flex flex-col justify-start w-full py-4 text-sm text-left" id={episode.guid}>
-			<h2 className="mb-2 text-2xl font-bold text-center text-brand-red md:mb-4">{episode.title}</h2>
+			<a href={`#${slug}`} className="cursor-pointer group !text-brand-red flex flex-row justify-center items-center mb-2 md:mb-4 gap-1">
+				<FontAwesomeIcon className="opacity-0 group-hover:opacity-100" icon={faLink} size="lg" />
+				<h2 id={slug} className="text-2xl font-bold text-center text-brand-red">
+					{episode.title}
+				</h2>
+			</a>
 			<div className="flex flex-col items-center justify-start gap-4 md:flex-row md:items-start">
 				<Image src={episode.imgSrc} alt={episode.title} className="w-32 rounded md:w-48 h-fit aspect-square" width={192} height={192} />
 				<div className="flex flex-col self-stretch overflow-hidden whitespace-break-spaces text-wrap text-ellipsis">
