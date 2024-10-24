9230647630234dab04fc82586f30a2562e258bdc
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-10-24 13:49:47 -0400

    headings

--- app/(pages)/time/TimeClient.tsx
+++ app/(pages)/time/TimeClient.tsx
@@ -10,11 +10,11 @@ export default function TimeClient() {
 			<div className="w-full p-2 overflow-hidden border rounded-lg border-brand-red bg-zinc-950/75">
 				<FitText compressor={0.75}>
 					<div className="flex flex-col items-center font-medium leading-none">
-						<h3 className="text-6xl font-semibold text-brand-red">Shawn (PST)</h3>
-						<h1>
+						<h2 className="text-6xl font-semibold text-brand-red">Shawn (PST)</h2>
+						<h3>
 							{/* <Clock format={'HH:mm:ss'} ticking={true} timezone={'US/Pacific'} /> */}
 							<Clock format={'h:mm A'} ticking={false} timezone={'US/Pacific'} />
-						</h1>
+						</h3>
 					</div>
 				</FitText>
 			</div>
@@ -22,11 +22,11 @@ export default function TimeClient() {
 			<div className="w-full p-2 overflow-hidden border rounded-lg border-brand-yellow bg-zinc-950/75">
 				<FitText compressor={0.75}>
 					<div className="flex flex-col items-center font-medium leading-none">
-						<h3 className="text-6xl font-semibold text-brand-yellow">Andy (BST)</h3>
-						<h1>
+						<h2 className="text-6xl font-semibold text-brand-yellow">Andy (BST)</h2>
+						<h3>
 							{/* <Clock format={'HH:mm:ss'} ticking={true} timezone={'Europe/London'} /> */}
 							<Clock format={'h:mm A'} ticking={false} timezone={'Europe/London'} />
-						</h1>
+						</h3>
 					</div>
 				</FitText>
 			</div>
@@ -34,18 +34,18 @@ export default function TimeClient() {
 			<div className="flex flex-col w-full px-2 overflow-hidden border divide-y rounded-lg divide-brand-blue border-brand-blue bg-zinc-950/75 [&>*]:py-2">
 				<FitText compressor={1.5}>
 					<div className="flex flex-col items-center font-medium leading-none">
-						<h3 className="text-4xl font-semibold text-brand-blue">Eastern US</h3>
-						<h1>
+						<h2 className="text-4xl font-semibold text-brand-blue">Eastern US</h2>
+						<h3>
 							<Clock format={'h:mm A'} ticking={false} timezone={'US/Eastern'} />
-						</h1>
+						</h3>
 					</div>
 				</FitText>
 				<FitText compressor={1.5}>
 					<div className="flex flex-col items-center font-medium leading-none">
-						<h3 className="text-4xl font-semibold text-brand-blue">Melbourne</h3>
-						<h1>
+						<h2 className="text-4xl font-semibold text-brand-blue">Melbourne</h2>
+						<h3>
 							<Clock format={'h:mm A'} ticking={false} timezone={'Australia/Melbourne'} />
-						</h1>
+						</h3>
 					</div>
 				</FitText>
 			</div>
