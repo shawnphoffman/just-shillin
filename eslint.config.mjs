950f0b9702e44ebf6320b58d6a7e586bbf91de50
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-10-28 16:11:49 -0400

    Upgrade eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import react from 'eslint-plugin-react'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all,
+})
+
+export default [
+	...compat.extends('@shawnphoffman/eslint-config', 'next/core-web-vitals'),
+	{
+		plugins: {
+			react,
+		},
+		rules: {
+			'react/no-unescaped-entities': 'warn',
+		},
+	},
+]
