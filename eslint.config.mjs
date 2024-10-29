6fc35d934043534b996122c3872196028bd21543
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-10-29 16:01:11 -0400

    lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,9 @@
 import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
+import shawnEslint from '@shawnphoffman/eslint-config/eslint.config.mjs'
 import react from 'eslint-plugin-react'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-
-import shawnEslint from '@shawnphoffman/eslint-config/eslint.config.mjs'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
